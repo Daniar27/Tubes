d1e40474d6c910bfd0cf9245c6eb22c2f74d4e88
Daniar27 <daniarmiftah@gmail.com>
2022-11-10 17:54:17 +0700

    <fix> Auth

--- Daniar/auth.c
+++ Daniar/auth.c
@@ -5,8 +5,8 @@
 
 FILE *file;
 
-int checkUsn,checkPsw = 0;
-char pass[20], password[20],user[20],username[20],ch;
+int checkUsn,checkPsw,opsi = 0;
+char pass[20], password[20],user[20],username[20];
 int auth(){
 	FILE *file;
 	file=fopen("auth.txt", "r");
@@ -15,20 +15,33 @@ int auth(){
 	
 	login:
 	//Login Activity
-	printf("Login\n");
-	printf("Username : ");
+	printf("=========================================\n");
+	printf("\tAplikasi Manajemen Inventory\n");
+	printf("=========================================\n");
+	printf("\n");
+	printf("\tLogin\n");
+	printf("\tUsername : ");
 	scanf("%s", &user);
-	printf("Password : ");
+	printf("\tPassword : ");
 	scanf("%s", &pass);	
+	
 		
 	checkUsn=strcmp(user,username);
 	checkPsw=strcmp(pass,password);
 	if(checkUsn !=0||checkPsw !=0){
 		
-		printf("nusername/password yang anda masukkan salah!\n");
-		getch();
-		system("cls");
-		goto login;
+		printf("\n\tusername/password yang anda masukkan salah!\n");
+		printf("\tPress 1 to Exit\n\tPress number except 1 to continue :");
+		scanf("%i", &opsi);
+		if(opsi == 1){
+			system("cls");
+			return CheckAuth;	
+		}
+		else{
+			system("cls");
+			goto login;
+		}
+		
 	}
 	else{
 		CheckAuth=1;
